7a0ae29fad7767d4d7fd0bde8b31b08c8739a34c
Yichao Yang <yichao@temporal.io>
2024-06-21 18:54:53 -0700

    Use VersionedTransition in StateMachineNode and Ref (#6183)
    ## What changed?
    <!-- Describe what has changed in this PR -->
    - Use VersionedTransition in StateMachineNode and Ref
    - Compare node last update failover version when syncing node. This is
    for addressing
    https://github.com/temporalio/temporal/pull/6145#discussion_r1646732338
    - Fix mutable state failover version in Ref for task refresh case. Need
    to use last versioned transition, instead of currentVersion, which can
    be newer.
    
    ## Why?
    <!-- Tell your future self why have you made these changes -->
    
    ## How did you test it?
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    - New & existing tests. 
    
    ## Potential risks
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    ## Documentation
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    ## Is hotfix candidate?
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->

--- service/history/workflow/mutable_state_impl_test.go
+++ service/history/workflow/mutable_state_impl_test.go
@@ -873,7 +873,7 @@ func (s *mutableStateSuite) buildWorkflowMutableState() *persistencespb.Workflow
 		TransitionHistory: []*persistencespb.VersionedTransition{
 			{
 				NamespaceFailoverVersion: failoverVersion,
-				MaxTransitionCount:       1024,
+				TransitionCount:          1024,
 			},
 		},
 	}
